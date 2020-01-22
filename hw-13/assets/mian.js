526c0b53375aab2de0649165fe164370455f00f4
Sergey <sergeykamialchuk@gmail.com>
2020-01-22 14:40:53 +0300

    fixed mistakes

--- hw-13/assets/mian.js
+++ hw-13/assets/mian.js
@@ -22,38 +22,33 @@ var data_users = [{
 
     let res_users = GetAllMatches(data_users, res_promt);
 
-    OutputToAlert(res_users);
+    outputToAlert(res_users);
 
     res_users = GetAllMatchesCaseInsensitive(data_users, res_promt);
 
-    OutputToAlert(res_users);
+    outputToAlert(res_users);
 }
 
 function GetAllMatches(data_users, res_promt) {
     let res = [];
-    data_users.forEach(user => {
-        if (user.lastName === res_promt) {
-            res.push(user);
-        }
-    });
+    res = data_users.filter(user => (user.lastName === res_promt));
     return res;
 }
 
 function GetAllMatchesCaseInsensitive(data_users, res_promt) {
     let res = [];
-    data_users.forEach(user => {
-        if (user.lastName.replace(/\s+/g, '').toUpperCase() === res_promt.replace(/\s+/g, '').toUpperCase()) {
-            res.push(user);
-        }
-    });
+    let funcCastLit = function (lastName) {
+        return lastName.replace(/\s+/g, '').toUpperCase();
+    }
+    res = data_users.filter(user => (funcCastLit(user.lastName) === funcCastLit(res_promt)));
     return res;
 }
 
-function OutputToAlert(res_users) {
+function outputToAlert(res_users) {
     if (res_users.length > 0) {
         let res_string = "";
         res_users.forEach(user => {
-            res_string += "user name: " + user.firstName + " " + user.lastName + ".\n" + "user age: " + user.age + ".\n\n";
+            res_string += `user name:  ${user.firstName}  ${user.lastName}.\nuser age: ${user.age}.\n\n`;
         });
         alert(res_string);
     } else {
